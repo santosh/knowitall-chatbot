d7818141a07eb6f68144efa7068ae41855ebaa03
Santosh Kumar <sntshkmr60@gmail.com>
2023-06-29 19:34:37 +0530

    Add chatbot

---
+++ env.js
@@ -0,0 +1,6 @@
+export const process = {
+    env: {
+        // following is a dummy API key
+        OPENAI_API_KEY: "sk-3irgISz5NJv2gizjr2YKTfaKekFJqiR8xilvpga6aaQvG9mt"
+    }
+}
---
+++ index.js
@@ -0,0 +1,64 @@
+import { Configuration, OpenAIApi } from 'openai'
+import { process } from './env'
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+delete configuration.baseOptions.headers['User-Agent']
+
+const openai = new OpenAIApi(configuration)
+
+const conversationArr = [
+    {
+        role: 'system',
+        content: 'You are a use assistant.'
+    }
+]
+
+const chatbotConversation = document.getElementById('chatbot-conversation')
+
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const userInput = document.getElementById('user-input')
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-human')
+    chatbotConversation.appendChild(newSpeechBubble)
+    newSpeechBubble.textContent = userInput.value
+
+    // push object to conversationArr
+    conversationArr.push({
+        role: 'user',
+        content: userInput.value
+    })
+
+    userInput.value = ''
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+
+    fetchReply()
+})
+
+async function fetchReply() {
+    const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: conversationArr
+    })
+    conversationArr.push(response.data.choices[0].message)
+    renderTypewriterText(response.data.choices[0].message.content)
+}
+
+function renderTypewriterText(text) {
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
+    chatbotConversation.appendChild(newSpeechBubble)
+    let i = 0
+    const interval = setInterval(() => {
+        newSpeechBubble.textContent += text.slice(i - 1, i)
+        if (text.length === i) {
+            clearInterval(interval)
+            newSpeechBubble.classList.remove('blinking-cursor')
+        }
+        i++
+        chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+    }, 50)
+}
